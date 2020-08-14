752d8d2e710c381b8026048b3cc642ae8378c159
Greg Nichols <gnichols@appliedres.com>
2020-08-14 16:01:53 -0400

    new files

--- routes/grades.js
+++ routes/grades.js
@@ -2,6 +2,29 @@ const express = require('express')
 const router = express.Router()
 const students = require('./students')
 
+const studentInfo = [
+  { 
+    id: 1,
+    name: 'student1',
+    details: 'details on student1',
+    grades: 75
+  },
+  { 
+    id: 2,
+    name: 'student2',
+    details: 'details on student2',
+    grades: 85
+
+  },
+  { 
+    id: 3,
+    name: 'student3',
+    details: 'details on student3',
+    grades: 95
+
+  }
+]
+
 
 router.get('/:id', (req, res) => {
   const studentId = parseInt(req.params.id)
@@ -13,7 +36,14 @@ router.get('/:id', (req, res) => {
 
 
 router.post('/:id/:grades', (req, res) => {
-    res.send('SUCCESS: you added '+req.params.id +'Grades: '+req.params.grades)
+ //   res.send('SUCCESS: you added '+req.params.id +'Grades: '+req.params.grades)
+    const grade = req.body
+    if (!grade.studentId)
+    res.status(422).send(() => {
+
+      return new Error('please include student ID in your request')
+    })
+    res.status(201).json(grade)
   })
 
 
